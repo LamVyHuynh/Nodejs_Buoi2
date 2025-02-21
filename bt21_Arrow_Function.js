5ab029f57b717a1955d86de8f6a55bf141225ef1
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-21 15:02:18 +0700

    bt21 arrow function

---
+++ bt21_Arrow_Function.js
@@ -0,0 +1,11 @@
+const countPrimes = (arr) => {
+  const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+  return arr.filter(isPrime).length;
+};
+console.log(countPrimes([1, 2, 3, 4, 5]));
